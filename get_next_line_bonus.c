70279ef0315ae42145c5207df884f9c9076973db
morisummer222 <jeapark@c3r8s6.42seoul.kr>
2022-07-23 16:21:17 +0900

    he

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jeapark <jeapark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 16:06:49 by jeapark           #+#    #+#             */
-/*   Updated: 2022/07/19 20:00:48 by jeapark          ###   ########seoul.kr  */
+/*   Updated: 2022/07/23 15:20:32 by jeapark          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,20 @@ char	*ft_strchr(const char *s, int c)
 	return (0);
 }
 
+/*
+	@param
+		(in)	buf_bak	"자를 문자열이 들어있는 변수"
+		(out)	line	"출력하기 위해 잘라낸 문자열이 들어가는 변수"
+		(out)	pos		"buf_bak문자열 내의 잘라낼 기준이 되는 문자'\n'의 위치를 나타내는 변수"
+	
+	@return
+		지정된 문자를 기준으로 잘라내 출력할 문자열이 담긴 주소
+
+	@detail
+		buf_bak의 문자열에서 첫번 째 개행 pos까지의 문자를 line 에 할당한다.
+		그리고 자르고 남은 문자는 tmp에 할당한 후 기존의 buf_bak를 할당 해제하고 아까 잘라내고 남은 문자열을 할당한 tmp를 buf_bak에 집어넣는다.
+*/
+
 static char	*ft_cut_line2(char **buf_bak, char **line, char *pos)
 {
 	char	*tmp;
@@ -52,7 +66,19 @@ static char	*ft_cut_line2(char **buf_bak, char **line, char *pos)
 	*buf_bak = tmp;
 	return (*line);
 }
+/*
+	@param
+		(in)	buf_bak	"자를 문자열이 들어있는 변수"
 
+	@return
+		지정된 문자를 기준으로 잘라내 출력할 문자열이 담긴 주소
+	
+	@detail
+		해당 주소가 있는지 그리고 빈문자열이 있는지 검사 후 문자열에 \n가 있는지 없는지 검사를 한다.
+		없다면 마지막 라인이지만 마지막에 \n가 없는것으로 간주한다. 하지만 개행 '\n'이 있다면
+		검사를 한 후 잘라내기 위해 ft_cut_line2()함수를 호출한다.
+		
+*/
 static char	*ft_cut_line(char **buf_bak)
 {
 	char	*pos;
@@ -78,6 +104,18 @@ static char	*ft_cut_line(char **buf_bak)
 	return (ft_cut_line2(buf_bak, &line, pos));
 }
 
+/*
+	@param 
+		(in) fd	"열려있는 파일디스크립터"
+
+	@return
+		할당된 문자열이 담긴 주소
+
+	#detail
+		해당 함수가 실행되기 적절한지 검사 후 문자를 BUFFER_SIZE만큼 읽어들인다
+		그리고 그 문자열 안에 '\n'가 있다면 주소 처음부터 해당 문자'\n'가 있는 문자열까지
+		자른다. 하지만 자를 문자'\n'이 보이지않는다면 보일때까지 읽어들인다.
+*/
 char	*get_next_line(int fd)
 {
 	static char	*buf_bak[OPEN_MAX];
